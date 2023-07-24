1915f5dab85df62e07b4527af3b0c1c44d4b7514
Aarav Arya <aarav.arya2307@gmail.com>
2023-07-24 15:41:57 +0530

    DOM

---
+++ index.js
@@ -0,0 +1,39 @@
+// // using ES6 import statement
+// import createPrompt from 'prompt-sync';
+// const prompt = createPrompt();
+// var name = prompt('ENTER: ');
+// console.log((name-2)*4 + 21);
+
+// function bmiCalculator(w, h){
+//     return Math.floor(w/(h*h));
+// }
+// console.log(bmiCalculator(65, 1.8));
+
+// function randomNumber (n1, n2){
+//     return Math.floor(Math.random()*100) + 1;
+// }
+
+// function fibo(n){
+//     if(n === 0 || n === 1) return n;
+//     return fibo(n-1) + fibo(n-2);
+// }
+// console.log(fibo(10));
+
+
+// function fibonacciGenerator (n) {
+//     //Do NOT change any of the code above 👆
+//         var output =[];
+//         //Write your code here:
+//         for(var i=0; i<=n; i++){
+//             if(i === 0 || i === 1) output.push(i);
+//             else{
+//                 output.push(output[output.length-1] + output[output.length-2]);
+//             }
+//         }
+//         return output;
+//         //Return an array of fibonacci numbers starting from 0.
+        
+//     //Do NOT change any of the code below 👇
+//     }
+// console.log(fibonacciGenerator(10));
+
