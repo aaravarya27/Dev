63b05ced9407c662cdb76eb40c4c616f242f2b73
Aarav Arya <aarav.arya2307@gmail.com>
2023-07-24 20:55:20 +0530

    Dice Game

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,17 @@
+var random1 = Math.floor(Math.random()*6) + 1;
+var random1path = "./images/" + "dice" + random1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", random1path);
+
+var random2 = Math.floor(Math.random()*6) + 1;
+var random2path = "./images/" + "dice" + random2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", random2path);
+
+if(random1 > random2){
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins";
+}
+else if(random2 > random1){
+    document.querySelector("h1").textContent = "Player 2 Wins 🚩";
+}
+else{
+    document.querySelector("h1").textContent = "Draw!";
+}
